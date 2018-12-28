504868e8c827062a0abaa08fde5388699984193d
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2018-12-28 15:46:54 +0700

    init commit test service

--- app/src/main/java/com/example/ntran/lazada/TrangChuActivity.kt
+++ app/src/main/java/com/example/ntran/lazada/TrangChuActivity.kt
@@ -1,12 +1,15 @@
 package com.example.ntran.lazada
 
+import android.net.Uri
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_trang_chu.*
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -17,10 +20,14 @@ class TrangChuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_trang_chu)
 
         btnGetData.setOnClickListener {
-            val maloaicha = edtMaLoaiCha.text
-            val duongDan = "http://192.168.1.2:81/weblazada/loaisanpham.php?maloaicha=$maloaicha"
+            val maloaicha = edtMaLoaiCha.text.toString()
+
+            //Đường dẫn loại get
+//            val duongDan = "http://192.168.1.2:81/weblazada/loaisanpham.php?maloaicha=$maloaicha"
+
+            val duongDan = "http://192.168.1.2:81/weblazada/loaisanpham.php"
             val downloadJSON = DownloadJSON()
-            downloadJSON.execute(duongDan)
+            downloadJSON.execute(duongDan,maloaicha)
         }
     }
 
@@ -30,8 +37,31 @@ class TrangChuActivity : AppCompatActivity() {
         override fun doInBackground(vararg params: String?): String? {
             //đường dẫn
             val url = URL(params[0])
+            
             //Điền đường dẫn vào trình duyệt
-            val connection = url.openConnection()
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            //Mở luồng
+            connection.doOutput = true
+            connection.doInput = true
+
+            val uri = Uri.Builder()
+            uri.appendQueryParameter("maloaicha",params[1])
+            val duLieuPost = uri.build().encodedQuery
+
+            val outputStream = connection.outputStream
+            val streamWriter = OutputStreamWriter(outputStream)
+            val writer = BufferedWriter(streamWriter)
+
+            //Ghi dữ liệu vào luồng
+            writer.write(duLieuPost)
+            writer.flush()
+
+            //Đóng luồng
+            writer.close()
+            streamWriter.close()
+            outputStream.close()
+
             //Nhấn enter
             connection.connect()
             //lấy dòng dữ liệu
@@ -47,6 +77,11 @@ class TrangChuActivity : AppCompatActivity() {
                 line = reader.readLine()
             }
             Log.e("kiemtra", data)
+
+            //Đóng luồng
+            inputStream.close()
+            streamReader.close()
+            reader.close()
             return data
         }
     }
