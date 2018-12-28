dfb0be593c8065d6a1ed9ae429af6e8efa16a051
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2018-12-28 16:18:31 +0700

    test checkout branch

--- app/src/main/java/com/example/ntran/lazada/View/TrangChu/TrangChuActivity.kt
+++ app/src/main/java/com/example/ntran/lazada/View/TrangChu/TrangChuActivity.kt
@@ -1,13 +1,14 @@
 package com.example.ntran.lazada.View.TrangChu
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.ntran.lazada.R
 import kotlinx.android.synthetic.main.activity_trang_chu.*
 import kotlinx.android.synthetic.main.app_bar_trang_chu.*
 
